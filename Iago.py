b7dc0ee4cc97064250e5cc5e527b517f004ca477
Daniel Renkert <drenkert@mymail.mines.edu>
2018-11-26 10:46:23 -0700

    Began making model. Need to get proper 8x8 output shape

--- Iago.py
+++ Iago.py
@@ -9,7 +9,22 @@ from tensorflow.keras import Model, Input
 
 def build_model():
     #multilayer model
-    
+    model = [ Conv2D(32,kernel_size=(3, 3), padding = 'same', activation='relu', input_shape = (28,28,1)),
+    Conv2D(32,kernel_size=(3, 3), padding = 'same', activation='relu'),
+    Conv2D(64,kernel_size=(3, 3), padding = 'same', activation='relu'),
+    Conv2D(64,kernel_size=(3, 3), padding = 'same', activation='relu'),
+    Conv2D(128,kernel_size=(3, 3), padding = 'same', activation='relu'),
+    Conv2D(128,kernel_size=(3, 3), padding = 'same', activation='relu'),
+    Conv2D(32,kernel_size=(3, 3), padding = 'same', activation='relu'),
+    Conv2D(32,kernel_size=(1, 1), padding = 'same', activation='relu'),
+    Dense(10, activation ='softmax')] #TODO sortof make this into an autoencoder to get proper output
+
+    cnn_model = Sequential(cnn_layers)
+    #raise NotImplementedError()
+
+    cnn_model.compile(optimizer="adam", loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+    cnn_model.fit(x_train.reshape(-1, 28, 28 ,1), y_train, epochs=1)
+
 
 def format_data():
     #stuff
